8b6a117a6bb0b9645b9eb3af6ffd0c89dd386b28
speerscbl <92505873+speerscbl@users.noreply.github.com>
2021-10-14 08:55:10 +0200

    update auxiliary.py

--- auxiliary.py
+++ auxiliary.py
@@ -1,25 +1,3 @@
-def agenda():
-	
-	return print("""
-1. Quick Introduction
-    - What is Python?
-    - Why is Python so popular?
-    - to IDEs / Jupyter Notebooks
-2. Python Basic Building Blocks
-    - Datatypes
-        - Strings, numbers booleans 
-	- Data Structures
-		- tuples
-		- lists
-		- dictionaries
-	-  Conditionals and Loops
-        - input validation (if/else)
-        - loop (while/for)
-	- functions
-3. Exercise
-4. Final
-	""")
-
 def exercise_2_tip_1():
 	return print("""
 Beginne zunächst mit der Definition. Es sollen 2 Werte übergeben werden:
@@ -37,8 +15,3 @@ def zahlen_checker(value, number):
 		return True
 	elif ...
 	""")
-
-
-
-# https://www.w3schools.com/python
-# https://realpython.com/
