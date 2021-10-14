15d4c2014f70d3f642b14cf1e34621a76784feb8
speerscbl <92505873+speerscbl@users.noreply.github.com>
2021-10-14 08:36:58 +0200

    initial commit

---
+++ auxiliary.py
@@ -0,0 +1,44 @@
+def agenda():
+	
+	return print("""
+1. Quick Introduction
+    - What is Python?
+    - Why is Python so popular?
+    - to IDEs / Jupyter Notebooks
+2. Python Basic Building Blocks
+    - Datatypes
+        - Strings, numbers booleans 
+	- Data Structures
+		- tuples
+		- lists
+		- dictionaries
+	-  Conditionals and Loops
+        - input validation (if/else)
+        - loop (while/for)
+	- functions
+3. Exercise
+4. Final
+	""")
+
+def exercise_2_tip_1():
+	return print("""
+Beginne zunächst mit der Definition. Es sollen 2 Werte übergeben werden:
+
+def zahlen_checker(value, number):
+...
+	""")
+	
+def exercise_2_tip_2():
+	return print("""
+Überlege dir nun eine Logik für den Fall "positiv" und 1.
+
+def zahlen_checker(value, number):
+	if value == "positiv" and number > 0:
+		return True
+	elif ...
+	""")
+
+
+
+# https://www.w3schools.com/python
+# https://realpython.com/
